ec739bacb253ce5dfa4ac20ea413d76d23c22a91
KyungsooLee <hector1210@naver.com>
2025-11-21 23:41:54 +0900

    11/21
    Managed Translate() to Addforce() of Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,38 +1,18 @@
-
 using UnityEngine;
 
-
-
 public class Bullet : MonoBehaviour
-
 {
-
     public float speed = 50f;
-
     public float lifeTime = 3f;
-
-
+    private Rigidbody rb;
 
     void Start()
-
     {
+        rb = GetComponent<Rigidbody>();
 
-        // Destroy the bullet after a certain time to clean up the scene
+        // 순간적인 힘(Impulse)을 가하여 발사
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
 
         Destroy(gameObject, lifeTime);
-
     }
-
-
-
-    void Update()
-
-    {
-
-        // Move the bullet forward
-
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-    }
-
 }
